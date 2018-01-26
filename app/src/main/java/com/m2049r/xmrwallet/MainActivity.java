a42aaa198475caad79737d07d43ca69f401bec1a
Stephan <stephan.hagios@gmail.com>
2018-01-26 22:43:40 +0100

    onboarding

---
+++ app/src/main/java/com/m2049r/xmrwallet/MainActivity.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018-2020 EarlOfEgo, m2049r
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.m2049r.xmrwallet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.m2049r.xmrwallet.onboarding.OnBoardingActivity;
+import com.m2049r.xmrwallet.onboarding.OnBoardingManager;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (OnBoardingManager.shouldShowOnBoarding(getApplicationContext()) || BuildConfig.DEBUG) {
+            startActivity(new Intent(this, OnBoardingActivity.class));
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        finish();
+    }
+}
---
+++ app/src/main/java/com/m2049r/xmrwallet/onboarding/OnBoardingAdapter.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2018-2020 EarlOfEgo, m2049r
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.m2049r.xmrwallet.onboarding;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.m2049r.xmrwallet.R;
+
+import timber.log.Timber;
+
+public class OnBoardingAdapter extends PagerAdapter {
+
+    interface Listener {
+        void setAgreeClicked(int position, boolean isChecked);
+
+        boolean isAgreeClicked(int position);
+
+        void setButtonState();
+    }
+
+    private final Context context;
+    private Listener listener;
+
+    OnBoardingAdapter(final Context context, final Listener listener) {
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup collection, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        final View view = inflater.inflate(R.layout.view_onboarding, collection, false);
+        final OnBoardingScreen onBoardingScreen = OnBoardingScreen.values()[position];
+
+        final Drawable drawable = ContextCompat.getDrawable(context, onBoardingScreen.getDrawable());
+        ((ImageView) view.findViewById(R.id.onboardingImage)).setImageDrawable(drawable);
+        ((TextView) view.findViewById(R.id.onboardingTitle)).setText(onBoardingScreen.getTitle());
+        ((TextView) view.findViewById(R.id.onboardingInformation)).setText(onBoardingScreen.getInformation());
+        if (onBoardingScreen.isMustAgree()) {
+            final CheckBox agree = ((CheckBox) view.findViewById(R.id.onboardingAgree));
+            agree.setVisibility(View.VISIBLE);
+            agree.setChecked(listener.isAgreeClicked(position));
+            agree.setOnClickListener(v -> {
+                listener.setAgreeClicked(position, ((CheckBox) v).isChecked());
+            });
+            listener.setButtonState();
+        }
+        collection.addView(view);
+        Timber.d("add " + position);
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return OnBoardingScreen.values().length;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup collection, int position, @NonNull Object view) {
+        Timber.d("destroy " + position);
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull final View view, @NonNull final Object object) {
+        return view == object;
+    }
+}
