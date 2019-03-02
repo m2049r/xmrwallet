ccb3ffb4869ee69035787636b50c59deb7505a1b
m2049r <m2049r@monerujo.io>
2019-03-02 09:06:19 +0100

    update slow_hash signature

--- app/src/main/cpp/monerujo.h
+++ app/src/main/cpp/monerujo.h
@@ -60,14 +60,14 @@ enum {
     HASH_DATA_AREA = 136
 };
 
-void cn_slow_hash(const void *data, size_t length, char *hash, int variant, int prehashed);
+void cn_slow_hash(const void *data, size_t length, char *hash, int variant, int prehashed, uint64_t height);
 
 inline void slow_hash(const void *data, const size_t length, char *hash) {
-    cn_slow_hash(data, length, hash, 0 /* variant */, 0/*prehashed*/);
+    cn_slow_hash(data, length, hash, 0 /*variant*/, 0 /*prehashed*/, 0 /*height*/);
 }
 
 inline void slow_hash_broken(const void *data, char *hash, int variant) {
-    cn_slow_hash(data, 200 /*sizeof(union hash_state)*/, hash, variant, 1 /*prehashed*/);
+    cn_slow_hash(data, 200 /*sizeof(union hash_state)*/, hash, variant, 1 /*prehashed*/, 0 /*height*/);
 }
 
 #ifdef __cplusplus
