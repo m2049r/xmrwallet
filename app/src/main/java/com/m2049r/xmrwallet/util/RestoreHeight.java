65ce9b0889e9a8394dd1f1b1e8384f837a029fb9
m2049r <m2049r@monerujo.io>
2020-06-01 10:04:09 +0200

    Update blockheight (#664)
    * update through june

--- app/src/main/java/com/m2049r/xmrwallet/util/RestoreHeight.java
+++ app/src/main/java/com/m2049r/xmrwallet/util/RestoreHeight.java
@@ -113,6 +113,9 @@ public class RestoreHeight {
         blockheight.put("2020-01-01", 2001315L);
         blockheight.put("2020-02-01", 2023656L);
         blockheight.put("2020-03-01", 2044552L);
+        blockheight.put("2020-04-01", 2066806L);
+        blockheight.put("2020-05-01", 2088411L);
+        blockheight.put("2020-06-01", 2110702L);
     }
 
     public long getHeight(String date) {
