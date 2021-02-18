86d26d15fbd6c53fcf218521dd5bf5bfe6413664
m2049r <m2049r@monerujo.io>
2021-02-18 13:29:44 +0100

    change build to docker

--- external-libs/script/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source script/env.sh
-
-rm -rf $EXTERNAL_LIBS_BUILD
-mkdir -p $EXTERNAL_LIBS_BUILD/src
--- external-libs/script/openssl-patch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source script/env.sh
-
-cp -a script/android-openssl $EXTERNAL_LIBS_BUILD_ROOT
