6ac82e29d374443bd87b0469011960ce6f15b26b
MajesticBank <97235900+MajesticBank@users.noreply.github.com>
2022-10-10 02:15:08 +0300

    Update DefaultNodes.java

--- app/src/main/java/com/m2049r/xmrwallet/data/DefaultNodes.java
+++ app/src/main/java/com/m2049r/xmrwallet/data/DefaultNodes.java
@@ -24,14 +24,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum DefaultNodes {
     MONERUJO("nodex.monerujo.io:18081"),
-    XMRTO("node.xmr.to:18081"),
     SUPPORTXMR("node.supportxmr.com:18081"),
+    MAJESTICBANK.SU("node.majesticbank.su:18089"),
+    MAJESTICBANK.IS("node.majesticbank.is:18089"),
     HASHVAULT("nodes.hashvault.pro:18081"),
     MONEROWORLD("node.moneroworld.com:18089"),
     XMRTW("opennode.xmr-tw.org:18089"),
     MONERUJO_ONION("monerujods7mbghwe6cobdr6ujih6c22zu5rl7zshmizz2udf7v7fsad.onion:18081/mainnet/monerujo.onion"),
     Criminales78("56wl7y2ebhamkkiza4b7il4mrzwtyvpdym7bm2bkg3jrei2je646k3qd.onion:18089/mainnet/Criminales78.onion"),
     xmrfail("mxcd4577fldb3ppzy7obmmhnu3tf57gbcbd4qhwr2kxyjj2qi3dnbfqd.onion:18081/mainnet/xmrfail.onion"),
+    MajesticBank("majesticrepik35vnngouksfl7jiwf6sj7s2doj3bvdffq27tgqoeayd.onion:18089/mainnet/majesticbank.onion"),
     boldsuck("6dsdenp6vjkvqzy4wzsnzn6wixkdzihx3khiumyzieauxuxslmcaeiad.onion:18081/mainnet/boldsuck.onion");
 
     @Getter
