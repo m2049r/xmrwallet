4b3b99ff2a516ef55690d831bbdb33482427cd47
m2049r <m2049r@monerujo.io>
2023-04-26 07:55:26 +0200

    fix colour of service logo & use it for receive qr code

--- app/src/main/java/com/m2049r/xmrwallet/ReceiveFragment.java
+++ app/src/main/java/com/m2049r/xmrwallet/ReceiveFragment.java
@@ -403,7 +403,7 @@ public class ReceiveFragment extends Fragment {
 
     private Bitmap getMoneroLogo() {
         if (logo == null) {
-            logo = Helper.getBitmap(getContext(), R.drawable.ic_monero_logo_b);
+            logo = Helper.getBitmap(getContext(), R.drawable.ic_monerujo);
         }
         return logo;
     }
