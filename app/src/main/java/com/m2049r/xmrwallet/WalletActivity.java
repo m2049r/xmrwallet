4da2106f04efdf0374d95c4d5045d059f2deda15
m2049r <m2049r@monerujo.io>
2023-04-30 17:16:20 +0200

    show active account in drawer

--- app/src/main/java/com/m2049r/xmrwallet/WalletActivity.java
+++ app/src/main/java/com/m2049r/xmrwallet/WalletActivity.java
@@ -1028,13 +1028,14 @@ public class WalletActivity extends BaseActivity implements WalletFragment.Liste
         final Wallet wallet = getWallet();
         if (wallet != null) {
             final int n = wallet.getNumAccounts();
+            final int currentAccount = getWallet().getAccountIndex();
             final boolean showBalances = (n > 1) && !isStreetMode();
             for (int i = 0; i < n; i++) {
                 final String label = (showBalances ?
                         getString(R.string.label_account, wallet.getAccountLabel(i), Helper.getDisplayAmount(wallet.getBalance(i), 2))
                         : wallet.getAccountLabel(i));
                 final MenuItem item = menu.add(R.id.accounts_list, getAccountId(i), 2 * i, label);
-                item.setIcon(R.drawable.ic_account_balance_wallet_black_24dp);
+                item.setIcon(i == currentAccount ? R.drawable.ic_outline_folder_open_24 : R.drawable.ic_outline_folder_24);
                 if (i == wallet.getAccountIndex())
                     item.setChecked(true);
             }
